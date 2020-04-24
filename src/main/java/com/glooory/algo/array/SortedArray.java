c0818f3a61342bdd32951d54296830c2f8be66c9
Glooory <glooorypu@gmail.com>
2020-04-24 14:40:27 +0800

    Mark Merge Sorted Array done

--- src/main/java/com/glooory/algo/array/SortedArray.java
+++ src/main/java/com/glooory/algo/array/SortedArray.java
@@ -63,6 +63,31 @@ public class SortedArray {
         return this.size;
     }
 
+    /**
+     * https://leetcode.com/problems/merge-sorted-array/
+     *
+     * Merge Sorted Array
+     * @param firstArray
+     * @param m
+     * @param secondArray
+     * @param n
+     * @return
+     */
+    public static void merge(int[] firstArray, int m, int[] secondArray, int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            firstArray[m++] = secondArray[i];
+            for (int j = m - 1; j > 0; j--) {
+                if (firstArray[j] < firstArray[j - 1]) {
+                    int temp = firstArray[j - 1];
+                    firstArray[j - 1] = firstArray[j];
+                    firstArray[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     private void ensureOpacity() {
         if (size >= data.length - 1) {
             grow();
