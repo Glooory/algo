8f0eb64880cd6ffcd535732d97277d2b7d66b1b6
Glooory <glooorypu@gmail.com>
2020-05-22 07:57:59 +0800

    Resolve subset with duplicates

--- src/main/java/com/glooory/algo/recursion/Subsets.java
+++ src/main/java/com/glooory/algo/recursion/Subsets.java
@@ -50,4 +50,58 @@ public class Subsets {
             tempList.remove(tempList.size() - 1);
         }
     }
+
+    /**
+     * 90. Subsets II https://leetcode.com/problems/subsets-ii/
+     *
+     * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+     *
+     * Note: The solution set must not contain duplicate subsets.
+     *
+     * Example:
+     *
+     * Input: [1,2,2]
+     * Output:
+     * [
+     *   [2],
+     *   [1],
+     *   [1,2,2],
+     *   [2,2],
+     *   [1,2],
+     *   []
+     * ]
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> subsetsWithDuplicates(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtrackWithDuplicates(nums, new ArrayList<>(), used, 0, result);
+
+        return result;
+    }
+
+    private static void backtrackWithDuplicates(int[] nums, List<Integer> tempList, boolean[] used, int start, List<List<Integer>> result) {
+        result.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            used[i] = true;
+            tempList.add(nums[i]);
+            backtrackWithDuplicates(nums, tempList, used, i + 1, result);
+            used[i] = false;
+            tempList.remove(tempList.size() - 1);
+        }
+    }
 }
