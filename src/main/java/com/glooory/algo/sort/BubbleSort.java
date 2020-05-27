933e6c5e615eb6a18e249d9621ce2d1c8370b9c6
Glooory <glooorypu@gmail.com>
2020-05-27 08:07:30 +0800

    Modify bubble sorting

--- src/main/java/com/glooory/algo/sort/BubbleSort.java
+++ src/main/java/com/glooory/algo/sort/BubbleSort.java
@@ -8,12 +8,18 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < nums.length; i++) {
+            // 提前退出冒泡循环的标志
+            boolean dataTransferred = false;
             for (int j = 0; j < nums.length - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
+                    dataTransferred = true; // 表示有数据交换
                 }
+            }
+            if (!dataTransferred) {
+                break; // 没有数据交换，提前退出
             }
         }
 
