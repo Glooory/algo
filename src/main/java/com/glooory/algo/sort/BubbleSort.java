85e0503d48b3ed8a301a9e2189bd08812a626a7d
Glooory <glooorypu@gmail.com>
2020-05-27 08:02:13 +0800

    Fix bubble sorting

--- src/main/java/com/glooory/algo/sort/BubbleSort.java
+++ src/main/java/com/glooory/algo/sort/BubbleSort.java
@@ -8,11 +8,11 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
--- src/test/java/com/glooory/algo/sort/BubbleSortTest.java
+++ src/test/java/com/glooory/algo/sort/BubbleSortTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
+    private int[] arr = new int[]{2, 7, 88, 311, 43, 5, 2373, 0, -2, 2, 34, 634563, 6436, 523, -322, 32, -34, 43, 762};
 
     @Test
     void sort() {
-        BubbleSort.printAll(BubbleSort.sort(new int[]{2, 7, 88, 311, 43, 5, 2373, 0, -2, 2}));
+        BubbleSort.printAll(BubbleSort.sort(arr));
     }
 }
